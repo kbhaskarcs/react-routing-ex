72ce0968ffe8216094a528b46dc82e74207346f1
kbhaskarcs <106826779+kbhaskarcs@users.noreply.github.com>
2024-10-18 11:00:30 +0530

    Add files via upload

---
+++ precache-manifest.0eb23d766f38736c0fd18576fd50584c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edd924ab7b0eba73941f67860500c18b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45b9b9edb02d381da001",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c229936d4d5ece39febe",
+    "url": "/static/js/2.de955918.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "45b9b9edb02d381da001",
+    "url": "/static/js/main.6c5b9d73.chunk.js"
+  },
+  {
+    "revision": "e21c8a56d64ede50141d",
+    "url": "/static/js/runtime~main.891e0007.js"
+  }
+]);
